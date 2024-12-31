1bc59a6e9e0286837b247a687b55b9ac85609e2f
Hannes Olschok <Hannes.o@t-online.de>
2024-12-31 01:06:16 +0100

    creating basic color-changing ui for tempProject

--- temp_project/static/code.js
+++ temp_project/static/code.js
@@ -1,1 +1,40 @@
-/*file for javascript*/
+const colorSlider = document.getElementById('colorSlider');
+const colorValue = document.getElementById('colorValue');
+const background = document.querySelector('.background');
+
+const colorGradient = [
+    'rgb(48, 48, 48)',
+    'rgb(79, 48, 79)',
+    'rgb(48, 48, 79)',
+    'rgb(48, 79, 48)',
+    'rgb(79, 79, 48)',
+    'rgb(79, 58, 48)',
+    'rgb(79, 48, 48)',
+    'rgb(48, 48, 48)'
+];
+
+
+function interpolateColor(value, colors) {
+    const index = Math.floor(value / (100 / (colors.length - 1)));
+    const remainder = (value % (100 / (colors.length - 1))) / (100 / (colors.length - 1));
+    
+    const startColor = colors[index];
+    const endColor = colors[index + 1];
+    
+    const [r1, g1, b1] = startColor.match(/\d+/g).map(Number);
+    const [r2, g2, b2] = endColor.match(/\d+/g).map(Number);
+    
+    const r = Math.round(r1 + remainder * (r2 - r1));
+    const g = Math.round(g1 + remainder * (g2 - g1));
+    const b = Math.round(b1 + remainder * (b2 - b1));
+    
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function updateBackgroundColor() {
+    const sliderValue = colorSlider.value;
+    const newColor = interpolateColor(sliderValue, colorGradient);
+    background.style.setProperty('--main-color', newColor);
+}
+
+colorSlider.addEventListener('input', updateBackgroundColor);
