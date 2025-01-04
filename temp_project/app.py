268c1d4b37c4650ac2779690077a64e78b0e71fe
Hannes Olschok <hannes.o@t-online.de>
2025-01-04 20:42:26 +0100

    [Bugfix] Update app.py -> html route + used imports

--- temp_project/app.py
+++ temp_project/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
 from routes import app_routes
 
 app = Flask(__name__)
 
 # Register routes from routes.py
 app.register_blueprint(app_routes)
+
+#main route
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
