4fcf003c052a9686cf6e3ea253e1a70b4f34cd12
Hannes Olschok <Hannes.o@t-online.de>
2025-01-06 20:20:43 +0100

    [Debug] adding update script to get newest version of OctaControl

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull || { echo "error: git pull failed"; exit 1; }
+
+chmod +x update.sh || { echo "error: chmod +x update.sh failed"; exit 1; }
+
+cd temp_project || { echo "error: cd temp_project failed"; exit 1; }
+
+python3 installDependencies.py || { echo "error: installDependencies.py failed"; exit 1; }
+
+sudo reboot || { echo "error: reboot failed"; exit 1; }
