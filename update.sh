8b3e2e744c85fb448aab9eeb19e41e336db97ec4
Hannes Olschok <Hannes.o@t-online.de>
2025-01-06 20:55:12 +0100

    [Bugfix] fixed update file

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ chmod +x update.sh || { echo "error: chmod +x update.sh failed"; exit 1; }
 
 cd temp_project || { echo "error: cd temp_project failed"; exit 1; }
 
-python3 installDependencies.py || { echo "error: installDependencies.py failed"; exit 1; }
+python3 installDependencies.py || { echo "error: installDependencies.py failed"; }
 
 sudo reboot || { echo "error: reboot failed"; exit 1; }
