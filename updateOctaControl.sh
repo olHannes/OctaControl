ec1e659288d8d1ae58433421026ee40c27e5022f
Hannes Olschok <Hannes.o@t-online.de>
2025-01-21 11:34:17 +0100

    [DEBUG]: updateOctaControl now changes the system settings from wayland to x11 => needed for unclutter [BUGFIX]: changed autostart script so the browser is startet in toggleable fullscreen

--- updateOctaControl.sh
+++ updateOctaControl.sh
@@ -1,12 +1,19 @@
-cd /home/hannes/Documents/OctaControl/temp_project/ || { echo "error cd the directory"; }
-git reset --hard || { echo "error while reset the repo"; }
-git pull || { echo "error pulling repo"; }
+#!/bin/bash
 
-python3 installDependencies.py || { echo "error installing dependencies"; }
+cd /home/hannes/Documents/OctaControl/temp_project/ || { echo "error cd the directory"; exit 1; }
 
-cd /home/hannes/Documents/OctaControl/ || { echo "echo returning the directory"; }
+git reset --hard || { echo "error while reset the repo"; exit 1; }
+git pull || { echo "error pulling repo"; exit 1; }
 
-chmod x addAutostart.sh || { echo "error chmod of addAutostart"; }
-bash addAutostart.sh || { echo "error exeuting addAutostart"; }
+python3 installDependencies.py || { echo "error installing dependencies"; exit 1; }
 
-exit 0;
+cd /home/hannes/Documents/OctaControl/ || { echo "error returning the directory"; exit 1; }
+
+chmod +x addAutostart.sh || { echo "error chmod of addAutostart"; exit 1; }
+bash addAutostart.sh || { echo "error executing addAutostart"; exit 1; }
+
+echo "Switching from Wayland to X11..."
+
+sudo raspi-config nonint do_gldriver G1 || { echo "error switching to X11"; exit 1; }
+
+exit 0
