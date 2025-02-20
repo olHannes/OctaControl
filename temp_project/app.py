3183e718703f38f95b6c3b5b9b5bbc817c169f83
HOlschok <holschok@gauselmann.de>
2025-02-20 14:51:52 +0100

    [Debug, Testing] implemented app as flask-socketIO

--- temp_project/app.py
+++ temp_project/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO
 from flask_cors import CORS
 from routes import *
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 # Register routes from routes.py
 app.register_blueprint(app_routes)
@@ -13,4 +15,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
