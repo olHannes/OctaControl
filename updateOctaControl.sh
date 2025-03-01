f39aceda2858a58fa748f8f5091e03ec26ce05d8
Hannes Olschok <hannes.o@t-online.de>
2025-03-01 17:17:34 +0100

    [Debug] added sudo rights for git

--- updateOctaControl.sh
+++ updateOctaControl.sh
@@ -2,8 +2,8 @@
 
 cd /home/hannes/Documents/OctaControl/temp_project/ || { echo "error cd the directory"; exit 1; }
 
-git reset --hard || { echo "error while reset the repo"; exit 1; }
-git pull || { echo "error pulling repo"; exit 1; }
+sudo git reset --hard || { echo "error while reset the repo"; exit 1; }
+sudo git pull || { echo "error pulling repo"; exit 1; }
 
 cd /home/hannes/Documents/OctaControl/ || { echo "error returning the directory"; exit 1; }
 chmod +x addAutostart.sh || { echo "error chmod of addAutostart"; exit 1; }
