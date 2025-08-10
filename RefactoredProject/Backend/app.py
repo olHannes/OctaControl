21b3f949047972fcbe3042953643b183dbf5df93
HOlschok <holschok@gauselmann.de>
2025-08-10 13:02:28 +0200

    implemented a basic app.py

---
+++ RefactoredProject/Backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+from api.wlan_setup import wlan_api
+from utils.Logger import Logger
+import os
+
+log = Logger()
+app = Flask(__name__, template_folder="../Frontend/templates", static_folder="../Frontend/static")
+CORS(app)
+
+app.register_blueprint(wlan_api)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    log.verbose("App", "Starte Flask Server")
+    app.run(host="0.0.0.0", port=5000, debug=True)
