b385e9220dc30396e66bf8527c3a2f8f223d71ad
HOlschok <holschok@gauselmann.de>
2025-08-14 10:05:13 +0200

    added climate_api to the flask-app

--- RefactoredProject/Backend/app.py
+++ RefactoredProject/Backend/app.py
@@ -7,6 +7,7 @@ from api.bluetooth.bluetooth_setup import bt_setup_api
 from services.relais_control import relais_api
 from services.system import system_api
 from services.volume_control import volume_api
+from services.climate import climate_api
 
 log = Logger()
 app = Flask(__name__, template_folder="../Frontend/templates", static_folder="../Frontend/static")
@@ -16,6 +17,7 @@ app.register_blueprint(volume_api)
 app.register_blueprint(wlan_api)
 app.register_blueprint(bt_setup_api)
 app.register_blueprint(relais_api)
+app.register_blueprint(climate_api)
 
 
 @app.route("/")
