8b5e5101b69c81679fad82ab210255031505e6de
Hannes Olschok <hannes.o@t-online.de>
2026-01-01 23:39:54 +0100

    started implementation of settings page

--- RefactoredProject/Frontend/restructuredProject/static/js/pages/settings.page.js
+++ RefactoredProject/Frontend/restructuredProject/static/js/pages/settings.page.js
@@ -1,3 +1,69 @@
 export function renderSettings(root) {
-  root.innerHTML = `<div class="card"><div class="card-title">Settings</div>Theme, Orte, Regeln…</div>`;
+  root.innerHTML = `
+    <section class="settings">
+      <div class="vertical-container centered-panel">
+        <h2 class="headline">Settings</h2>
+        <p class="description">Manage system preferences and connectivity</p>
+
+        <span class="subheading">Connectivity</span>
+        
+        <div class="panel">
+          <div class="icon-wrapper">
+            <span class="icon icon--bluetooth"></span>
+          </div>
+          <div class="vertical-container align-left">
+            <h3>Bluetooth</h3>
+            <span>Connect to audio devices</span>
+          </div>
+          <input type="checkbox" id="bluetoothToggle" checked />
+        </div>
+
+        <div class="panel">
+          <div class="icon-wrapper">
+            <span class="icon icon--network"></span>
+          </div>
+          <div class="vertical-container align-left">
+            <h3>Wi-Fi</h3>
+            <span>Connect to wireless networks</span>
+          </div>
+          <input type="checkbox" id="wifiToggle"/>
+        </div>
+
+        <span class="subheading">System</span>
+
+        <div class="panel">
+          <div class="icon-wrapper">
+            <span class="icon icon--network"></span>
+          </div>
+          <div class="vertical-container align-left">
+            <h3>System Update</h3>
+            <span>Check for Updates</span>
+          </div>
+          <span>\></span>
+        </div>
+
+        <div class="panel">
+          <div class="icon-wrapper">
+            <span class="icon icon--network"></span>
+          </div>
+          <div class="vertical-container align-left">
+            <h3>Restart</h3>
+            <span>Restart the infotainment system</span>
+          </div>
+          <span>\></span>
+        </div>
+
+        <div class="panel">
+          <div class="icon-wrapper">
+            <span class="icon icon--network"></span>
+          </div>
+          <div class="vertical-container align-left">
+            <h3>Shutdown</h3>
+            <span>Power off the system</span>
+          </div>
+          <span>\></span>
+        </div>
+      </div>
+    </section>
+  `;
 }
